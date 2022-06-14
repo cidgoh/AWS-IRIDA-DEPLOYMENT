d7ade5e2e9d1d346eada5b9ea58fe9285e871a60
despean <despean15@gmail.com>
2022-06-14 15:27:30 -0700

    kube ingress update

--- DEPLOYMENT/aws/main.tf
+++ DEPLOYMENT/aws/main.tf
@@ -149,14 +149,3 @@ module "irida" {
 #    endpoint = module.irida.endpoint
 #  }
 #}
-
-module "alb_ingress_controller" {
-  source  = "iplabs/alb-ingress-controller/kubernetes"
-  version = "3.4.0"
-
-  k8s_cluster_type = "eks"
-  k8s_namespace    = "kube-system"
-
-  aws_region_name  = data.aws_region.current.name
-  k8s_cluster_name = data.aws_eks_cluster.cluster.name
-}
